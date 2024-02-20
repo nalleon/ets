25a86996e96ede86d2f8edad5d1fc6329b7dcca5
nalleon <nabil14716@gmail.com>
2024-02-20 15:33:36 +0000

    Se refactoriza el repositorio

---
+++ unidad-7/tareas/junit-test/junit-test/junit-test/src/main/java/es/iespuerto/ets/modelo/Libro.java
@@ -0,0 +1,73 @@
+package es.iespuerto.ets.modelo;
+
+import java.util.Objects;
+
+public class Libro {
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private String fPublicacion;
+
+    public  Libro (){}
+
+    public Libro(String isbn, String titulo, String autor, String fPublicacion) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.fPublicacion = fPublicacion;
+    }
+
+    /**
+     * Getters y setters
+     */
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getfPublicacion() {
+        return fPublicacion;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setfPublicacion(String fPublicacion) {
+        this.fPublicacion = fPublicacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "isbn='" + isbn + '\'' +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", fPublicacion='" + fPublicacion + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Libro libro = (Libro) o;
+        return Objects.equals(isbn, libro.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn);
+    }
+}
---
+++ unidad-7/tareas/junit-test/junit-test/junit-test/src/test/java/es/iespuerto/ets/modelo/LibroTest.java
@@ -0,0 +1,54 @@
+package es.iespuerto.ets.modelo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LibroTest {
+    private String isbn= "isbn";
+    private String titulo = "nombre";
+
+    private String autor = "autor";
+    private String fPublicacion="20/02/2024";
+
+    Libro libro = new Libro();
+    @BeforeEach
+    public void beforeEach(){
+        libro = new Libro(isbn, titulo, autor, fPublicacion);
+    }
+
+    @Test
+    public void libroNotNullTest(){
+        Assertions.assertNotNull(libro, "La variable no puede ser nula.");
+    }
+
+    @Test
+    public void libroGetSetTest(){
+        String autorUpdate = "autorUpdate";
+        String fPublicacionUpdate ="21/02/2024";
+        String tituloUpdate = "tituloUpdate";
+        libro.setAutor(autorUpdate);
+        libro.setTitulo(tituloUpdate);
+        libro.setfPublicacion(fPublicacionUpdate);
+        Assertions.assertEquals(tituloUpdate, libro.getTitulo(), "Resultado no esperado");
+        Assertions.assertEquals(autorUpdate, libro.getAutor(), "Resultado no esperado");
+        Assertions.assertEquals(fPublicacionUpdate, libro.getfPublicacion(), "Resultado no esperado");
+        Assertions.assertEquals(isbn, libro.getIsbn(), "Resultado no esperado");
+
+    }
+
+    @Test
+    public void toStringTest(){
+        Assertions.assertTrue(libro.toString().contains(isbn), "No contiene el valor esperado");
+        Assertions.assertTrue(libro.toString().contains(titulo), "No contiene el valor esperado");
+        Assertions.assertTrue(libro.toString().contains(fPublicacion), "No contiene el valor esperado");
+        Assertions.assertTrue(libro.toString().contains(autor), "No contiene el valor esperado");
+    }
+
+    @Test
+    public void equalsTest(){
+
+    }
+
+}
